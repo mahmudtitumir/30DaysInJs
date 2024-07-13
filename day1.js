406e4766fcdd1020399c63b07dfee36fadfd2614
mahmudtitumir <titumir947@gmail.com>
2024-07-13 11:04:43 +0600

    day 01 complete

---
+++ day1.js
@@ -0,0 +1,27 @@
+// variable declar
+var num = 2024;
+console.log(num);
+
+let str = "chaiaurcode";
+console.log(str);
+
+const isHave = false;
+console.log(isHave);
+
+// data types
+let numType = 55;
+let strType = "jschallenge";
+let boolType = true;
+let objType = {
+    day: 1,
+    isComplete: true,
+};
+let arrType = [15, "50", false, 21];
+console.log(numType, strType, boolType, objType, arrType);
+
+//Reassign
+str = "js";
+// isHave = true;
+
+console.log("Reassign: ", str);
+// console.log(isHave); // Output: TypeError
